9a71693c68caf3838aeb46667212bbd386470786
aymanema18 <aymanema18@gmail.com>
2024-07-10 21:35:38 +0100

    Make the necessary files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './style.css';
+import img1 from './images/img1.png';
+import img2 from './images/img2.png';
+import img3 from './images/img3.png';
+import img4 from './images/img4.png';
+
+const image1 = document.querySelector('.img-1');
+const image2 = document.querySelector('.img-2');
+const image3 = document.querySelector('.img-3');
+const image4 = document.querySelector('.img-4');
+
+image1.src = img1;
+image2.src = img2;
+image3.src = img3;
+image4.src = img4;
