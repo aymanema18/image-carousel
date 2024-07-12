ce22b97fa5330daa9cb0d0ab78ff599c06c0f8a6
aymanema18 <aymanema18@gmail.com>
2024-07-12 01:32:03 +0100

    Make the images and circles change

--- src/index.js
+++ src/index.js
@@ -8,8 +8,51 @@ const image1 = document.querySelector('.img-1');
 const image2 = document.querySelector('.img-2');
 const image3 = document.querySelector('.img-3');
 const image4 = document.querySelector('.img-4');
+const imgCircle1 = document.querySelector('.img1-div');
+const imgCircle2 = document.querySelector('.img2-div');
+const imgCircle3 = document.querySelector('.img3-div');
+const imgCircle4 = document.querySelector('.img4-div');
+const circles = document.querySelectorAll('.circle');
 
 image1.src = img1;
 image2.src = img2;
 image3.src = img3;
 image4.src = img4;
+document.querySelector('.img1-div').style.backgroundColor = 'gray';
+
+function runCarousel() {
+    if (image1.style.display !== 'none') {
+        image1.style.display = 'none';
+        image1.nextElementSibling.style.display = 'block';
+        document.querySelector('.img1-div').style.backgroundColor = '';
+        document.querySelector(
+            '.img1-div',
+        ).nextElementSibling.style.backgroundColor = 'gray';
+    } else if (image1.style.display === 'none') {
+        let next = image1;
+        do {
+            next = next.nextElementSibling;
+        } while (next.style.display === 'none');
+        next.style.display = 'none';
+        if (next === document.querySelector('img:last-child')) {
+            image1.style.display = 'block';
+            document.querySelector('.circle:last-child').style.backgroundColor =
+                '';
+            document.querySelector('.img1-div').style.backgroundColor = 'gray';
+            return;
+        }
+        circles.forEach((circle) => {
+            if (
+                circle.getAttribute('data-id') ===
+                next.nextElementSibling.getAttribute('data-id')
+            ) {
+                circle.style.backgroundColor = 'gray';
+            } else {
+                circle.style.backgroundColor = '';
+            }
+        });
+        next.nextElementSibling.style.display = 'block';
+    }
+}
+
+setInterval(runCarousel, 2000);
