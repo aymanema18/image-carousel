32aa0d23cfe82045e1e94a050355c3bce0065dd1
aymanema18 <aymanema18@gmail.com>
2024-07-12 16:35:55 +0100

    Add arrows

--- src/index.js
+++ src/index.js
@@ -13,12 +13,105 @@ const imgCircle2 = document.querySelector('.img2-div');
 const imgCircle3 = document.querySelector('.img3-div');
 const imgCircle4 = document.querySelector('.img4-div');
 const circles = document.querySelectorAll('.circle');
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
 
 image1.src = img1;
 image2.src = img2;
 image3.src = img3;
 image4.src = img4;
 document.querySelector('.img1-div').style.backgroundColor = 'gray';
+
+leftArrow.addEventListener('click', () => {
+    let flag = false;
+    circles.forEach((circle) => {
+        if (circle.style.backgroundColor !== '' && flag === false) {
+            const images = document.querySelectorAll('.img');
+            for (let img of images) {
+                if (
+                    img.getAttribute('data-id') ===
+                    circle.getAttribute('data-id')
+                ) {
+                    img.style.display = 'none';
+                    circle.style.backgroundColor = '';
+                    if (img.previousElementSibling) {
+                        img.previousElementSibling.style.display = 'block';
+                        circle.previousElementSibling.style.backgroundColor =
+                            'gray';
+                        clearInterval(intr);
+                        intr = setInterval(runCarousel, 5000);
+                        flag = true;
+                    } else {
+                        document.querySelector(
+                            '.img:last-child',
+                        ).style.display = 'block';
+                        document.querySelector(
+                            '.circle:last-child',
+                        ).style.backgroundColor = 'gray';
+                        clearInterval(intr);
+                        intr = setInterval(runCarousel, 5000);
+                        flag = true;
+                    }
+                }
+            }
+        }
+    });
+});
+
+rightArrow.addEventListener('click', () => {
+    let flag = false;
+    circles.forEach((circle) => {
+        if (circle.style.backgroundColor !== '' && flag === false) {
+            const images = document.querySelectorAll('.img');
+            for (let img of images) {
+                if (
+                    img.getAttribute('data-id') ===
+                    circle.getAttribute('data-id')
+                ) {
+                    img.style.display = 'none';
+                    circle.style.backgroundColor = '';
+                    if (img.nextElementSibling) {
+                        img.nextElementSibling.style.display = 'block';
+                        circle.nextElementSibling.style.backgroundColor =
+                            'gray';
+                        clearInterval(intr);
+                        intr = setInterval(runCarousel, 5000);
+                        flag = true;
+                    } else {
+                        document.querySelector('.img-1').style.display =
+                            'block';
+                        document.querySelector(
+                            '.img1-div',
+                        ).style.backgroundColor = 'gray';
+                        clearInterval(intr);
+                        intr = setInterval(runCarousel, 5000);
+                        flag = true;
+                    }
+                }
+            }
+        }
+    });
+});
+
+circles.forEach((circle) => {
+    const images = document.querySelectorAll('.img');
+    for (let img of images) {
+        if (img.getAttribute('data-id') === circle.getAttribute('data-id')) {
+            circle.addEventListener('click', () => {
+                images.forEach((img) => {
+                    img.style.display = 'none';
+                });
+                img.style.display = 'block';
+                circles.forEach((circle) => {
+                    circle.style.backgroundColor = '';
+                });
+                circle.style.backgroundColor = 'gray';
+                clearInterval(intr);
+                intr = setInterval(runCarousel, 5000);
+            });
+        }
+    }
+});
 
 function runCarousel() {
     if (image1.style.display !== 'none') {
@@ -55,4 +148,4 @@ function runCarousel() {
     }
 }
 
-setInterval(runCarousel, 2000);
+let intr = setInterval(runCarousel, 5000);
